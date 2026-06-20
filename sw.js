6669a387ae974424ce448054ac274ab33bc5b55e
fajar-arch-civil <ufajar80@gmail.com>
2026-06-21 01:04:01 +0700

    Somes Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rt08-v4';
+const CACHE = 'rt08-v5';
 const SHELL=['./','./index.html','./lomba.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e=>{
