24153aface60c491e184f7503ade4523c5d5e248
fajar-arch-civil <ufajar80@gmail.com>
2026-06-22 00:25:21 +0700

    Lomba Update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rt08-v18';
+const CACHE = 'rt08-v19';
 const SHELL=['./','./index.html','./lomba.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e=>{
