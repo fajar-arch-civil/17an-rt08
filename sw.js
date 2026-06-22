f4d125e20c75d254ad2a95e4640a72e98adb04cd
fajar-arch-civil <ufajar80@gmail.com>
2026-06-23 00:31:04 +0700

    Panitia Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rt08-v42';
+const CACHE = 'rt08-v43';
 const SHELL=['./','./index.html','./lomba.html','./manifest.json','./icon-192.png','./icon-512.png','./panitia.html'];
 
 self.addEventListener('install', e=>{
