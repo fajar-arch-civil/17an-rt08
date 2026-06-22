b814104c7ae23c9defa0e180bdd7f55a30f00b7a
fajar-arch-civil <ufajar80@gmail.com>
2026-06-22 13:35:13 +0700

    Big Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rt08-v23';
+const CACHE = 'rt08-v24';
 const SHELL=['./','./index.html','./lomba.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e=>{
